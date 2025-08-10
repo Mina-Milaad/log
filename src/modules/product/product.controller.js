1065aa15e368ef620214fcd44e96ee744da4836a
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-10 15:50:09 +0300

    deploy

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -5,7 +5,7 @@ import { Product } from "../../../database/models/product.model.js";
 const addProduct = catchError(async (req, res, next) => {
     let product = new Product(req.body);
     await product.save();
-    res.status(201).json({ message: "Product added successfully", product });
+    res.status(201).json({ message: "Productt added successfully", product });
 })
 
 
