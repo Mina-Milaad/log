c61c6c41c0c1d73e25f548fcb65c3fc1673c0369
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-10 16:47:25 +0300

    last commit

--- src/modules/bootstrap.js
+++ src/modules/bootstrap.js
@@ -1,6 +1,6 @@
 import productRouter from './product/product.routes.js';
 
 export const bootstrap = (app) => {
-    app.use('products', productRouter);
+    app.use('/products', productRouter);
     
 }
