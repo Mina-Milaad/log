b72b36693862250e2b8b8ff2ad74f467e125a663
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-11 10:59:19 +0300

    commit

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+
+export const verifyToken = (req, res, next) => {
+    let { token } = req.headers
+    jwt.verify(token, "process.env.JWT_KEY", (err, decoded) => {
+        if (err) return res.status(401).json({ message: "invaled token", err })
+
+        req.user = decoded
+        console.log(req.user);
+        next()
+
+    })
+
+
+
+
+}
