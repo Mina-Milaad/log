8f7c89d1a2de2a8d0af04ac2808e4df9f43f6714
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-18 15:11:05 +0300

    test

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
     let { token } = req.headers
-    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+    jwt.verify(token, "process.env.JWT_KEY", (err, decoded) => {
         if (err) return res.status(401).json({ message: "invaled token", err })
 
         req.user = decoded
